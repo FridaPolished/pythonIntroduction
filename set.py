e402c383b04e57a7dbd17d2107765fd9b9bd9479
FridaPolished <fridapulido@gmail.com>
2020-01-17 13:49:53 -0800

    Notes on sets

---
+++ set.py
@@ -0,0 +1,11 @@
+#sets are objects that hold unique values
+
+set1 = {"cat", "dog", "rat"}
+
+
+print(set1) #printing the set does not guarantee the order of the elements
+print("cat" in set1) #returns boolean for inclusion
+ 
+set2 = {"horse", "dragon", "cat"}
+
+print(set1.intersection(set2)) #
