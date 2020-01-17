c47883498665f48e5c95ad8b49b32dd1281498c8
FridaPolished <fridapulido@gmail.com>
2020-01-17 12:52:28 -0800

    tuples notes on Python

---
+++ tuples.py
@@ -0,0 +1,17 @@
+#tuples are a sequence of Python objects.
+#they are inmutable but they can be useful because they respond
+#to most operations related to strings and to numbers
+#tuples are defined with ()
+#useful when we need to iterate and access to the information without modifying it
+tuple_1 = ('cat', 'dog', 'rat')
+tuple_2 = tuple_1 + tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+# tuple_1[0] = "bird"
+# print(tuple_1)
+# print(tuple_2)
+
+#we cant remove values or modify them in any way but we can create new
+#tuples
